0e60f86a7a2cfcd01b6fe60acd70ce19f3b47793
WongChoice <ankitdasrock@gmail.com>
2022-12-29 18:09:33 +0530

    fs

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('button-above-title');
+    button.addEventListener('click', function() {
+      // Your code here
+      console.log('Button clicked');
+    });
+  });
+  
